46013641ece9a11267c5e39f6762484540a8c501
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-04-02 13:00:36 +0300

    remove unnecessary import

--- services/uaa/src/main/java/com/github/punchat/uaa/account/jsr303/PasswordValidator.java
+++ services/uaa/src/main/java/com/github/punchat/uaa/account/jsr303/PasswordValidator.java
@@ -1,7 +1,5 @@
 package com.github.punchat.uaa.account.jsr303;
 
-import lombok.extern.slf4j.Slf4j;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
