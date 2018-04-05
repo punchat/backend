79c815aef96476a1a7b6613a201f7a6d679ff059
Aleksey Kobzev <a.kobzev@aol.com>
2018-04-05 18:39:10 +0300

    create example rabbitmq

--- services/config/src/main/java/com/github/punchat/config/ConfigApplication.java
+++ services/config/src/main/java/com/github/punchat/config/ConfigApplication.java
@@ -2,10 +2,11 @@ package com.github.punchat.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude = { RabbitAutoConfiguration.class })
 public class ConfigApplication {
 
     public static void main(String[] args) {
