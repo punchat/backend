0885fc0bdcf35af0641f6eb0439d96796f772f2b
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-04-26 23:28:59 +0300

    add swagger

---
+++ services/messaging/src/main/java/com/github/punchat/messaging/security/ResourceServerConfig.java
@@ -0,0 +1,24 @@
+package com.github.punchat.messaging.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/v2/api-docs",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html**",
+                        "/webjars/**",
+                        "favicon.ico"
+                ).permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
