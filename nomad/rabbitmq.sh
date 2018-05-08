c95e086021f89b27f15f99b23cf52951565a88f4
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-05-09 01:45:07 +0300

    expose rabbitmq ports change uaa application name

--- nomad/rabbitmq.sh
+++ nomad/rabbitmq.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -h rabbitmq --name rabbitmq --network=punchat --network-alias=rabbitmq rabbitmq:management
+docker run -d -p 15672:15672 -p 5672:5672 -h rabbitmq --name rabbitmq --network=punchat --network-alias=rabbitmq rabbitmq:management
