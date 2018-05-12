794cbaa109bd82c3b772e6d93be1c2400bfd04f8
Aleksey Kobzev <a.kobzev@aol.com>
2018-05-12 12:50:11 +0300

    edit swagger handlers

--- services/am/src/main/java/com/github/punchat/am/domain/invite/workspace/WorkspaceInviteController.java
+++ services/am/src/main/java/com/github/punchat/am/domain/invite/workspace/WorkspaceInviteController.java
@@ -1,10 +1,14 @@
 package com.github.punchat.am.domain.invite.workspace;
 
 import com.github.punchat.dto.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value="workspace", description="Operations for Workspace invitation")
 public class WorkspaceInviteController {
     private final WorkspaceInviteService service;
 
@@ -12,25 +16,28 @@ public class WorkspaceInviteController {
         this.service = service;
     }
 
-    @PostMapping("/workspace/members/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createWorkspaceInvite(@RequestBody WorkspaceInvitation invitation) {
+    @ApiOperation(value = "Create new invite to workspace")
+    @PostMapping(value = "/workspace/members/", produces = "application/json")
+    public ResponseEntity createWorkspaceInvite(@RequestBody WorkspaceInvitation invitation) {
        service.createWorkspaceInvite(invitation);
+       return new ResponseEntity("Invite to workspace created successfully", HttpStatus.CREATED);
     }
 
-    @PutMapping("/workspace/members/check")
+    @ApiOperation(value = "Validation workspace invite by email")
+    @PutMapping(value = "/workspace/members/check", produces = "application/json")
     public WorkspaceEmailValidationResult checkWorkspaceInvite(@RequestBody WorkspaceEmailValidation email) {
         return service.checkWorkspaceInvite(email);
     }
 
-    @PutMapping("/workspace/codes/request")
-    @ResponseStatus(HttpStatus.OK)
-    public void requestAccessCode(@RequestBody NewAccessCodeRequest email) {
+    @ApiOperation(value = "Request new access code for invitation")
+    @PutMapping(value = "/workspace/codes/request", produces = "application/json")
+    public ResponseEntity requestAccessCode(@RequestBody NewAccessCodeRequest email) {
         service.requestAccessCode(email);
+        return new ResponseEntity("Access code requested successfully", HttpStatus.OK);
     }
 
-
-    @PutMapping("/workspace/codes/check")
+    @ApiOperation(value = "Check access code for invitation")
+    @PutMapping(value = "/workspace/codes/check", produces = "application/json")
     public WorkspaceAccessCodeValidationResult checkAccessCode(@RequestBody WorkspaceAccessCodeValidation accessCode) {
         return service.checkAccessCode(accessCode);
     }
--- services/am/src/main/java/com/github/punchat/am/swagger/SwaggerConfig.java
+++ services/am/src/main/java/com/github/punchat/am/swagger/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.SecurityScheme;
@@ -32,7 +33,7 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .securityContexts(Collections.singletonList(securityContext))
