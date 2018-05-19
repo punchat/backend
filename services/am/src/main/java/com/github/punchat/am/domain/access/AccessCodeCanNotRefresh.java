4f049a90ee31a5dfbe1b06a3ea8f53324cd38160
Aleksey Kobzev <a.kobzev@aol.com>
2018-05-19 19:45:30 +0300

    edit error response to forbidden

--- services/am/src/main/java/com/github/punchat/am/domain/access/AccessCodeCanNotRefresh.java
+++ services/am/src/main/java/com/github/punchat/am/domain/access/AccessCodeCanNotRefresh.java
@@ -3,7 +3,7 @@ package com.github.punchat.am.domain.access;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class AccessCodeCanNotRefresh extends RuntimeException {
     public AccessCodeCanNotRefresh(Long minutes) {
         super(String.format("Access code can not be refreshed. Please try again after %d minutes", minutes));
