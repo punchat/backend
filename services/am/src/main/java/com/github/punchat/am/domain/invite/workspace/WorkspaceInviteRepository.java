787906fd03dd298e93bec5a3e098559db68104a8
Aleksey Kobzev <a.kobzev@aol.com>
2018-05-20 20:39:14 +0300

    delete artifacts of moving

--- services/am/src/main/java/com/github/punchat/am/domain/invite/workspace/WorkspaceInviteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.punchat.am.domain.invite.workspace;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkspaceInviteRepository extends JpaRepository<WorkspaceInvite, Long> {
-    WorkspaceInvite findByEmail(String email);
-
-    boolean existsByEmail(String email);
-}
