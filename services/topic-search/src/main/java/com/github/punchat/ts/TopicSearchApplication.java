f9fc6c2b2918d86ae7e8d053472c07708270ab32
Yaroslav <32554867+Darkparty@users.noreply.github.com>
2018-05-27 13:26:20 +0300

    init topic-search microservice

---
+++ services/topic-search/src/main/java/com/github/punchat/ts/TopicSearchApplication.java
@@ -0,0 +1,13 @@
+package com.github.punchat.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicSearchApplication.class, args);
+    }
+
+}
