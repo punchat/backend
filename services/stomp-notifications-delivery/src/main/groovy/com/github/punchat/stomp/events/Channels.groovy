b71a7fe7721c1743902e1dfe8c48ce6930eeac5d
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-06-09 17:15:16 +0300

    fix channels

--- services/stomp-notifications-delivery/src/main/groovy/com/github/punchat/stomp/events/Channels.groovy
+++ services/stomp-notifications-delivery/src/main/groovy/com/github/punchat/stomp/events/Channels.groovy
@@ -1,15 +1,15 @@
 package com.github.punchat.stomp.events
 
-import org.springframework.cloud.stream.annotation.Output
+import org.springframework.cloud.stream.annotation.Input
 import org.springframework.messaging.SubscribableChannel
 
 interface Channels {
     String NEW_BROADCAST_MESSAGE_EVENTS = "newBroadcastMessageEvents"
     String NEW_DIRECT_MESSAGE_EVENTS = "newDirectMessageEvents"
 
-    @Output(Channels.NEW_BROADCAST_MESSAGE_EVENTS)
+    @Input(Channels.NEW_BROADCAST_MESSAGE_EVENTS)
     SubscribableChannel newBroadcastMessageEvents()
 
-    @Output(Channels.NEW_DIRECT_MESSAGE_EVENTS)
+    @Input(Channels.NEW_DIRECT_MESSAGE_EVENTS)
     SubscribableChannel newDirectMessageEvents()
 }
